757b25f2efc6806bc3a445cd2844308c5a0ae72c
Naruse <xnarunarux@gmail.com>
2022-05-15 20:16:19 +0900

    Keymap settings.

--- keyboards/handwired/splitbox/config.h
+++ keyboards/handwired/splitbox/config.h
@@ -7,9 +7,8 @@
 #define MATRIX_ROWS 8   // Rows are doubled-up
 #define MATRIX_COLS 7
 
-#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
-//#define UNUSED_PINS { }
+#define MATRIX_ROW_PINS { D1, D4, D7, B4 }
+#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5 }
 
 // Diode setting
 #define DIODE_DIRECTION COL2ROW
