5890a9905bc0a9a1bbbe597bdbd6b6bb20238b85
Naruse <xnarunarux@gmail.com>
2022-05-16 00:07:09 +0900

    Enable extrakey.

--- keyboards/handwired/splitbox/rules.mk
+++ keyboards/handwired/splitbox/rules.mk
@@ -1,9 +1,9 @@
 MCU 				= atmega32u4
 BOOTLOADER 			= caterina
 
-BOOTMAGIC_ENABLE 	= no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE 	= no        # Enable Bootmagic Lite
 MOUSEKEY_ENABLE 	= no        # Mouse keys
-EXTRAKEY_ENABLE 	= no        # Audio control and System control
+EXTRAKEY_ENABLE 	= yes       # Audio control and System control
 CONSOLE_ENABLE 		= no        # Console for debug
 COMMAND_ENABLE 		= no        # Commands for debug and configuration
 NKRO_ENABLE 		= yes       # Enable N-Key Rollover
